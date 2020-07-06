ecfdd6482aebadb941fe6a257be7c5959b122513
Hadeer <hadeerelnaggar@hotmail.com>
2020-07-06 23:05:15 +0200

    added missing view files and code reviewed

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,5 +1,8 @@
 var projectModel = require("../models/project");
 var clientModel = require("../models/client");
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+var sendemail = require('../sendmail') 
 
 module.exports.home = async function (req, res) {
     if (!req.session.cid) {
@@ -15,17 +18,66 @@ module.exports.home = async function (req, res) {
         res.render('client/home', { data: results });
     }
 };
+module.exports.clientLogin = async function (req, res) {
+    var result = await clientModel.getClientByEmail(req.body.email);
+    if(result=== null){
+        res.send("Invalid email or password")
+    }
+    else{
+        bcrypt.compare(req.body.password, result.password)
+				.then(match => {
+					if (!match) {
+						res.send("Invalid email or password")
+					}
+					else { //Add admin name and email to the session
+						req.session.user = result.name;
+						req.session.email = result.email;
+						req.session.cid = result.clientId;
+						req.session.save()
+						res.send("successfully loggedin")
+					}
+				})
+    }
 
-exports.clientLogin = function (req, res) {
-    clientModel.login(req, res);
 };
-exports.sendResetPasswordEmail = function (req, res) {
-    clientModel.forgetPassword(req, res)
+module.exports.sendResetPasswordEmail = async function (req, res) {
+    // clientModel.forgetPassword(req, res)
+    var result = await clientModel.getClientByEmail(req.query.email)
+    if(result === null){
+        res.send("user not found")
+    }
+    else{
+        crypto.randomBytes(32, async (err, buffer) => {
+            if (err) console.log(err)
+            else {
+                const token = buffer.toString("hex")
+                var email = req.query.email
+                var expireToken = Date.now() + 3600000
+                var output = await clientModel.updateTokenAndExpireTokenOfClient(token,expireToken,email)
+                var emailsubject = "please reset your password"
+                var emailtext = "Please use the following link to reset your password \n"
+                + req.protocol + '://' + req.get('host') + "/resetpassword/" + token + "?email=" + email + "&type=client"
+                sendemail.sendToNewAdmin(email, emailsubject, emailtext)
+                res.redirect('/')
+                }
+            })
+        }
 }
-exports.resetPassword = function (req, res) {
-    clientModel.resetPassword(req, res)
-}
-exports.editPassword = function (req, res) {
-    clientModel.editPassword(req, res)
+module.exports.resetPassword = async function (req, res) {
+    // clientModel.resetPassword(req, res)
+    var result = await clientModel.getClientByEmailAndToken(req.body.email,req.body.token)
+    if (result === null) {
+        res.send("no such user")
+    }
+    else {
+        var expireToken = result.expireToken;
+        if (parseInt(expireToken) >= parseInt(Date.now().toString())) {
+            var hashedpassword = await bcrypt.hash(req.body.password, 12)
+            var output = await clientModel.updatePasswordOfClient(req.body.email,hashedpassword)
+            console.log(output)
+            res.send("password reset")
+            //elmafrood troo7 l 7eta
+        }
+    }
 }
 
--- models/client.js
+++ models/client.js
@@ -1,8 +1,5 @@
 const util = require('util');
 const con = require('../database');
-const bcrypt = require('bcryptjs')
-var sendemail = require('../sendmail') 
-const crypto = require('crypto')
 const query = util.promisify(con.query).bind(con);
 
 module.exports.getClientById = async function (id) {
@@ -14,63 +11,38 @@ module.exports.getClientById = async function (id) {
 		return output[0];
 	return null;
 };
-
-module.exports.login = function (req, res) {
-	var checkStatus = true;
-	//get user firstly by email
-	con.query("select * from client where email = \'" + req.body.email + "\'", function (err, result, field) {
-		if (err) throw err;
-		else if (result.length == 0) {
-			res.send("Invalid email or password")
-			checkStatus = false;
-		}
-		if (checkStatus) {
-			//compare given password from req with the hashed password saved in database
-			bcrypt.compare(req.body.password, result[0].password)
-				.then(match => {
-					if (!match) {
-						res.send("Invalid email or password")
-					}
-					else { //Add admin name and email to the session
-						req.session.user = result[0].name;
-						req.session.email = result[0].email;
-						req.session.cid = result[0].clientId;
-						req.session.save()
-						res.send("successfully loggedin")
-					}
-				})
-		}
-	});
+module.exports.getClientByEmail = async function(email){
+	let sql = "SELECT * FROM client WHERE email = ? ";
+	let inserts = [email]
+	sql= con.format(sql,inserts);
+	const output = await query(sql);
+	if (output.length === 1)
+		return output[0];
+	return null;
+};
+module.exports.getClientByEmailAndToken = async function(email,token){
+	let sql = "select * from client where email = ? and resetPasswordToken = ? ";
+	let inserts = [email,token]
+	sql= con.format(sql,inserts);
+	const output = await query(sql);
+	if (output.length === 1)
+		return output[0];
+	return null;
 }
-
-module.exports.resetPassword = function (req, res) {
-	var newpassword = req.body.password
-	var email = req.body.email
-	var token = req.body.token
-	console.log(token)
-	con.query("select * from client where email = \'" + email + "\' and resetPasswordToken = \'" + token + "\'", function (err, result, field) {
-		if (err) throw err
-		else if (result.length == 0) {
-			res.send("no such user")
-		}
-		else {
-			console.log("bye")
-			var expireToken = result[0].expireToken;
-			if (parseInt(expireToken) >= parseInt(Date.now().toString())) {
-				bcrypt.hash(newpassword, 12)
-					.then(hashedpassword => {
-						con.query("update client set password = \'"
-							+ hashedpassword + "\',resetPasswordToken = null , expireToken = null where email = \'" + email +
-							"\'", function (err, result, field) {
-								if (err) throw err
-								res.send("password reset")
-							})
-					})
-			}
-		}
-	})
+module.exports.updatePasswordOfClient = async function(email,newpassword){
+	let sql ="update client set password = ? , resetPasswordToken = null , expireToken = null where email = ? "
+	let inserts = [newpassword,email]
+	sql= con.format(sql,inserts);
+	const output = await query(sql);
+	return output;
+} 
+module.exports.updateTokenAndExpireTokenOfClient = async function(token,expiretoken,email){
+	let sql = "update client set resetPasswordToken = ? , expireToken = ? where email = ? "
+	let inserts = [token,expiretoken,email]
+	sql = con.format(sql,inserts)
+	const output = await query(sql);
+	return output;
 }
-
 module.exports.forgetPassword = function (req, res) {
 	con.query("select * from client where email = \'" + req.query.email + "\'", function (err, result, field) {
 		if (err) throw err;
@@ -82,7 +54,6 @@ module.exports.forgetPassword = function (req, res) {
 				if (err) console.log(err)
 				else {
 					const token = buffer.toString("hex")
-					console.log(token)
 					var email = req.query.email
 					var expireToken = Date.now() + 3600000
 					con.query("update client set resetPasswordToken = \'"
@@ -90,15 +61,13 @@ module.exports.forgetPassword = function (req, res) {
 						"\'", function (err, result, field) {
 							if (err) throw err
 							else {
-								console.log("send email")
 								var emailsubject = "please reset your password"
 								var emailtext = "Please use the following link to reset your password \n"
 									+ req.protocol + '://' + req.get('host') + "/resetpassword/" + token + "?email=" + email + "&type=client"
 								sendemail.sendToNewAdmin(email, emailsubject, emailtext)
-								res.send("please check your email to reset password")
+								res.redirect('/')
 							}
 						})
-
 				}
 			});
 		}
